91fd91bbc524b03ff39de5432c2305576ae29311
johnb432 <58661205+johnb432@users.noreply.github.com>
2022-02-25 17:28:58 +0100

    1.1.0.2

--- addons/main/CfgMagazineWells.hpp
+++ addons/main/CfgMagazineWells.hpp
@@ -1,6 +1,6 @@
 class CfgMagazineWells {
     class CBA_12g_6rnds {
-        WB_Magazines[] += {
+        MAGAZINE_WELL[] += {
             "ACE_6Rnd_12Gauge_Pellets_No0_Buck",
             "ACE_6Rnd_12Gauge_Pellets_No1_Buck",
             "ACE_6Rnd_12Gauge_Pellets_No2_Buck",
@@ -11,7 +11,7 @@ class CfgMagazineWells {
     };
 
     class CBA_12g_8rnds {
-        WB_Magazines[] += {
+        MAGAZINE_WELL[] += {
             "ACE_8Rnd_12Gauge_Pellets_No00_Buck",
             "ACE_8Rnd_12Gauge_Pellets_No0_Buck",
             "ACE_8Rnd_12Gauge_Pellets_No1_Buck",
@@ -24,7 +24,7 @@ class CfgMagazineWells {
     };
 
     class CBA_300WM_AI {
-        WB_Magazines[] += {
+        MAGAZINE_WELL[] += {
             "ACE_20Rnd_762x67_Mk248_Mod_0_Mag",
             "ACE_20Rnd_762x67_Mk248_Mod_1_Mag",
             "ACE_20Rnd_762x67_Berger_Hybrid_OTM_Mag"
@@ -32,7 +32,7 @@ class CfgMagazineWells {
     };
 
     class CBA_300WM_AICS {
-        WB_Magazines[] += {
+        MAGAZINE_WELL[] += {
             "ACE_20Rnd_762x67_Mk248_Mod_0_Mag",
             "ACE_20Rnd_762x67_Mk248_Mod_1_Mag",
             "ACE_20Rnd_762x67_Berger_Hybrid_OTM_Mag"
@@ -40,20 +40,20 @@ class CfgMagazineWells {
     };
 
     class CBA_338LM_AI {
-        WB_Magazines[] += {
+        MAGAZINE_WELL[] += {
             "ACE_10Rnd_338_300gr_HPBT_Mag",
             "ACE_10Rnd_338_API526_Mag"
         };
     };
 
     class CBA_65x39_Katiba {
-        WB_Magazines[] += {
+        MAGAZINE_WELL[] += {
             "ACE_30Rnd_65x39_caseless_green_mag_Tracer_Dim"
         };
     };
 
     class CBA_65x39_MX {
-        WB_Magazines[] += {
+        MAGAZINE_WELL[] += {
             "ACE_30Rnd_65x39_caseless_mag_Tracer_Dim",
             "ACE_30Rnd_65x47_Scenar_mag",
             "ACE_30Rnd_65_Creedmor_mag"
@@ -61,13 +61,13 @@ class CfgMagazineWells {
     };
 
     class CBA_65x39_MX_XL {
-        WB_Magazines[] += {
+        MAGAZINE_WELL[] += {
             "ACE_100Rnd_65x39_caseless_mag_Tracer_Dim"
         };
     };
 
     class CBA_50BMG_100rnds {
-        WB_Magazines[] += {
+        MAGAZINE_WELL[] += {
             "ace_csw_100Rnd_127x99_mag",
             "ace_csw_100Rnd_127x99_mag_green",
             "ace_csw_100Rnd_127x99_mag_red",
